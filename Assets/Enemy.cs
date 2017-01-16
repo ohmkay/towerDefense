a6f035846d0b00ce18ab01cc06a5b59f1fc4d4b6
ohmkay <caleb.w.sweeney@gmail.com>
2017-01-16 09:40:16 -0800

    c# files

---
+++ Assets/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wayPointIndex = 0;
+
+	void Start ()
+    {
+        Waypoints waypoints = GameObject.FindObjectOfType<Waypoints>();
+        target = Waypoints.waypoints[0];		
+	}
+	
+	void Update ()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+
+	}
+}
---
+++ Assets/Waypoints.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+    public static Transform[] waypoints;
+
+    void Awake ()
+    {
+        waypoints = new Transform[transform.childCount];
+
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            transform.GetChild(i);
+        }
+    }
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
